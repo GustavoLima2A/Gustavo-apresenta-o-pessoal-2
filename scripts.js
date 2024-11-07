baaaadaf03709230491b50189d36f7c4dfd26a6b
Gustagol-07 <00001206255055sp@al.educacao.sp.gov.br>
2024-11-07 15:40:54 -0300

    ..

--- scripts.js
+++
@@ -1,6 +0,0 @@
-// Exemplo de animação simples
-document.addEventListener('DOMContentLoaded', () => {
-    const header = document.querySelector('header');
-    header.style.transition = 'transform 1s ease';
-    header.style.transform = 'scale(1.1)';
-});
